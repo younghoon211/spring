28336d2dc74cc6d605ebd1ba835056b9cc05070f
younghoon211@naver.com <younghoon211@naver.com>
2022-09-15 20:37:44 +0900

    refactor: 생성자 주입 방식으로 변경 -> DIP 법칙 위배x

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig { // app 실제 동작에 필요한 구현 객체를 생성하는 클래스
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+
+
+}
