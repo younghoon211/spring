523c3795415ca1bd1e5e452a4110cbb4a0a6a6c2
younghoon211@naver.com <younghoon211@naver.com>
2022-09-21 21:24:33 +0900

    feat: 스프링 컨테이너 생성

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,15 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+
+        // AppConfig에서 @Bean이 붙은 클래스들을 스프링 컨테이너에 등록
+        ApplicationContext ac = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
