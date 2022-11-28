f8516096d5994b5f9c9acc4680f09a0017153d6c
seok <younghoon211@naver.com>
2022-11-28 17:26:56 +0900

    test: 테스트코드 오류 수정

--- core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -22,7 +22,6 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면, 중복 오류가 발생한다")
     void findBeanByParentTypeDuplicate() {
-        DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
         assertThrows(NoUniqueBeanDefinitionException.class,
                 () -> ac.getBean(DiscountPolicy.class)
         );
