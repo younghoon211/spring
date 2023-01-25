6ddaf2064a3566de6612a62d3be9211bc2bccab0
seok <younghoon211@naver.com>
2023-01-25 17:33:03 +0900

    test: 회원 관리 로직 구현 테스트

---
+++ mvc1/servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,49 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        // 전체 테스트 시 테스트 순서에 따라 결과에 영향 -> clearStore() 필수!
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // given 주어졌을 때
+        Member member = new Member("member", 20);
+
+        // when 실행했을 때
+        Member savedMember = memberRepository.save(member);
+
+        // then 결과
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
