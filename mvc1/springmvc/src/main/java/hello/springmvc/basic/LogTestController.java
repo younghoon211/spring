3bf8db233528201b15f1395476494caced261400
seok <younghoon211@naver.com>
2023-02-01 22:05:16 +0900

    feat: 간단한 로깅 기능 구현

---
+++ mvc1/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    // 여기에서 getClass() == LogTestController.class
+    // private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping
+    public String logTest() {
+        String name = "spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        // RestController에서 문자 리턴하면 문자 그대로 리턴
+        return "ok";
+    }
+
+}
