13942a8473cc632622137489d4087dfbc148db9e
seok <younghoon211@naver.com>
2023-02-06 01:13:23 +0900

    refactor: 예제 수정(v3) 및 추가(v4)

--- mvc1/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ mvc1/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -1,6 +1,8 @@
 package hello.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,9 +40,27 @@ public class RequestBodyStringController {
         responseWriter.write("ok");
     }
 
-    @ResponseBody
+    /**
+     * HttpEntity: HTTP header, body 정보를 편리하게 조회
+     * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * 응답에서도 HttpEntity 사용 가능
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     */
     @PostMapping("/request-body-string-v3")
-    public String requestBodyStringV3(@RequestBody String messageBody) {
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+        HttpHeaders messageHeader = httpEntity.getHeaders();
+        String messageBody = httpEntity.getBody();
+        log.info("messageHeader={}", messageHeader);
+        log.info("messageBody={}", messageBody);
+        return new HttpEntity<>("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) {
         log.info("messageBody={}", messageBody);
         return "ok";
     }
