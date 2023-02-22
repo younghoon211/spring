a861d1da0f8802284868dc148e97fa3cdb4a463b
seok <younghoon211@naver.com>
2023-02-22 16:46:10 +0900

    feat: Bean Validation - groups 기능 구현

--- mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,17 +10,18 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class) // only 수정 시
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min=1000, max=1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min=1000, max=1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class) // only 저장 시
     private Integer quantity;
 
     public Item() {
