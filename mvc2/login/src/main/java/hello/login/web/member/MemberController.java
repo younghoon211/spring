243049f8950510e4c41b79a14619b4cb0884e2a8
seok <younghoon211@naver.com>
2023-03-12 17:45:49 +0900

    chore: @Validated -> @Valid로 수정

--- mvc2/login/src/main/java/hello/login/web/member/MemberController.java
+++ mvc2/login/src/main/java/hello/login/web/member/MemberController.java
@@ -5,11 +5,12 @@ import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -25,7 +26,7 @@ public class MemberController {
     }
 
     @PostMapping("/add")
-    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
+    public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "members/addMemberForm";
         }
